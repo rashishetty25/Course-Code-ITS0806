9a02755a7ee6c57ef940accbfc9eb20202173aa8
rashishetty25 <rashishetty25@gmail.com>
2024-02-22 20:33:43 -0500

    Made changes to filter.py

--- filter.py
+++ filter.py
@@ -1,7 +1,14 @@
 def filter_even_numbers(numbers):
     return [num for num in numbers if num % 2 == 0]
 
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-even_numbers = filter_even_numbers(numbers)
-print("Even numbers:", even_numbers)
+def filter_odd_numbers(numbers):
+    return [num for num in numbers if num % 2 != 0]
 
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+even_numbers = filter_even_numbers(numbers)
+odd_numbers = filter_odd_numbers(numbers)
+
+print("Original numbers:", numbers)
+print("Even numbers:", even_numbers)
+print("Odd numbers:", odd_numbers)
